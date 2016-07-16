7cfd5b267f82b9771ccf50fa3dc0892aba1ddd2a
Eric Dallo <ericdallo06@hotmail.com>
2016-07-16 18:08:58 -0300

    Add migratus

--- project.clj
+++ project.clj
@@ -10,12 +10,24 @@
             [ring/ring-defaults "0.2.1"]
             [ring/ring-devel "1.5.0"]
             [selmer "1.0.7"]
+            [korma "0.4.2"]
+            [mysql/mysql-connector-java "5.1.6"]
+            [migratus "0.8.13"]
+            [migratus-lein "0.4.0"]
           ]
   :plugins [
   					[lein-sassy "1.0.7"]
+  					[migratus-lein "0.3.7"]
   				 ]
   :sass {:src "resources/sass" 
   			 :dst "resources/public/css"
   			 :style :compressed}
   :hooks [leiningen.sass]
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:classname "com.mysql.jdbc.Driver"
+                  :subprotocol "mysql"
+                  :subname "//localhost/followthatsite"
+                  :user "root"
+                  :password ""}}
   :main followthatsite.core)
---
+++ resources/migrations/1-create-users-table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS user(
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` varchar(60) NOT null,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `UK_user_name`(`name`)
+);
